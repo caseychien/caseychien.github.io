97f7a3257bfa6dd5cc16f05e84529ae6153dd281
Casey Chien <case.chien@gmail.com>
2018-08-29 21:44:35 -0700

    add second tab and code cleanup

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-
+$('nav a').click(function(e) {
+	console.log('click');
+	console.log(this);
+	console.log(this.id);
+	e.preventDefault();
+	$('nav a').removeClass('active');
+	$(this).addClass('active');
+	if(this.id === !'exp'){
+	  $('.exper').addClass('noshow');
+	}
+	else if(this.id === 'exp') {
+	  $('.exper').removeClass('noshow');
+	  $('.content').children().not('.exper').addClass('noshow');
+	}
+	else if (this.id === 'prof') {
+	  $('.profile').removeClass('noshow');
+	   $('.content').children().not('.profile').addClass('noshow');
+	}
 });
